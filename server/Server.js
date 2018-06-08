ee824c87391c6950348038223104ab548e14deb7
Kirill Romanov <kirillik95@yandex.ru>
2018-06-08 18:00:15 +0300

    Problem with sql queries

--- server/Server.js
+++ server/Server.js
@@ -7,9 +7,18 @@ let connection = mysql.createConnection({
     password : 'sT16ui24st16ui24'
   });
 
+  //connecting to mysql
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+    //checking that db not exists (else drop database) and create new
+    console.log(connection.query("DROP DATABASE IF EXISTS `reactMegaNews`;"));
+    console.log(connection.query("CREATE DATABASE `reactMegaNews` DEFAULT CHARACTER SET utf8;"));
+    console.log(connection.query("CREATE TABLE `reactMegaNews`.`category` (`category_id` INT NOT NULL,`name` VARCHAR(45) NULL,PRIMARY KEY (`category_id`));"));
+    console.log(connection.query("CREATE TABLE `reactMegaNews`.`country` (`country_id` INT NOT NULL,`name` VARCHAR(45) NOT NULL,PRIMARY KEY (`country_id`));"));
+    console.log(connection.query("CREATE TABLE `reactMegaNews`.`source` (`source_id` INT NOT NULL,`name` VARCHAR(45) NOT NULL,PRIMARY KEY (`source_id`));"));
+    console.log(connection.query("CREATE TABLE `reactMegaNews`.`news` (`news_id` INT NOT NULL,`news_title` VARCHAR(45) NOT NULL,`news_desc` VARCHAR(45) NOT NULL,`news_author` VARCHAR(45) NOT NULL,`news_url` VARCHAR(45) NOT NULL,`news_imageUrl` VARCHAR(45) NOT NULL,`news_pubdate` DATETIME NOT NULL DEFAULT NOW(),`news_category` INT NOT NULL,`news_country` INT NOT NULL,`news_source` INT NOT NULL,PRIMARY KEY (`news_id`));"));
+    console.log(connection.query("ALTER TABLE `reactMegaNews`.`news` ADD INDEX `fk_news_category_idx` (`news_category` ASC), ADD INDEX `fk_news_country_idx` (`news_country` ASC), ADD INDEX `fk_news_source_idx` (`news_source` ASC); ALTER TABLE `reactMegaNews`.`news` ADD CONSTRAINT `fk_news_category` FOREIGN KEY (`news_category`) REFERENCES `reactMegaNews`.`category` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE, ADD CONSTRAINT `fk_news_country` FOREIGN KEY (`news_country`) REFERENCES `reactMegaNews`.`country` (`country_id`) ON DELETE CASCADE ON UPDATE CASCADE, ADD CONSTRAINT `fk_news_source` FOREIGN KEY (`news_source`) REFERENCES `reactMegaNews`.`source` (`source_id`) ON DELETE CASCADE ON UPDATE CASCADE;"));
   });
 
 //-------------------------------------------------
